6fae731c8f1dc9fc346c083d60bae19756ad7b59
hamishrichardson1 <156303131+hamishrichardson1@users.noreply.github.com>
2024-07-08 17:30:12 +0100

    Widen profile and handle 512x512

--- hazenlib/tasks/acr_slice_thickness.py
+++ hazenlib/tasks/acr_slice_thickness.py
@@ -84,6 +84,8 @@ class ACRSliceThickness(HazenTask):
             tuple: x and y coordinates of ramp
         """
         # X
+        print(f'res = {res}')
+
         investigate_region = int(np.ceil(5.5 / res[1]).item())
 #        print(f'investigate_region is {investigate_region}')
         if np.mod(investigate_region, 2) == 0:
@@ -116,7 +118,8 @@ class ACRSliceThickness(HazenTask):
         # take rough estimate of x points for later line profiles
         #x = np.round([np.min(width_pts) + 0.2 * width, np.max(width_pts) - 0.2 * width])
         # x limits of profile fail in sagittal view due to air-bubble. Fix these values as will not vary much (HR 26.04.2024)
-        x = [85,165]
+
+        x = [round(50/res[1]),round(200/res[1])]
         # Y
         c = skimage.measure.profile_line(
             img,
